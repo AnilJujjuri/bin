133102b98490d14a309415fc8f8f686061c370a1
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-07 08:29:14 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,39 +1,27 @@
 import can
 
-def receive_can_messages(bus):
-
-    while True:
-
-        message = bus.recv()
-
-        print("Received message:", message)
-
 def main():
+    # Create a CAN bus interface
+    bus = can.Bus(interface='socketcan', channel='can0')
 
-    # Create a CAN bus object
+    try:
+        # Send a CAN message
+        message = can.Message(arbitration_id=0x123, data=[0x01, 0x02, 0x03])
+        bus.send(message)
 
-    bus = can.interface.Bus(channel='can0', bustype='socketcan')
+        # Receive CAN messages
+        while True:
+            received_message = bus.recv(timeout=1.0)
+            if received_message is None:
+                break
+            print("Received message:", received_message)
 
-    # Start a separate thread to receive CAN messages
+    except can.CanError as e:
+        print("CAN error:", str(e))
 
-    receiver_thread = can.ThreadSafeBus(bus, receive_own_messages=True)
+    finally:
+        # Close the CAN bus interface
+        bus.shutdown()
 
-    receiver_thread.start()
-
-    # Send a CAN message
-
-    message = can.Message(arbitration_id=0x123, data=[0x01, 0x02, 0x03])
-
-    bus.send(message)
-
-    # Wait for a while to receive messages
-
-    input("Press Enter to stop receiving messages...\n")
-
-    # Stop the receiver thread
-
-    receiver_thread.stop()
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     main()
