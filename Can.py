97868c5ffbb230d3adc66491ca0aee0cf35abb1f
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-07 09:11:08 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,31 +1,33 @@
 import can
 
+def send_can_message(bus, arbitration_id, data):
+    message = can.Message(arbitration_id=arbitration_id, data=data)
+    bus.send(message)
+
+def receive_can_messages(bus):
+    while True:
+        message = bus.recv(timeout=1.0)
+        if message is None:
+            break
+        print("Received message:", message)
+
 def main():
     # Create a CAN bus interface
-    bus = can.Bus(interface='socketcan', channel='can0')
+    bus = can.interface.Bus(bustype='virtual', channel='can0')
 
     try:
         # Send a CAN message
-        message = can.Message(arbitration_id=0x123, data=[0x01, 0x02, 0x03])
-        bus.send(message)
+        send_can_message(bus, arbitration_id=0x123, data=[0x01, 0x02, 0x03])
 
         # Receive CAN messages
-        while True:
-            received_message = bus.recv(timeout=1.0)
-            if received_message is None:
-                break
-            print("Received message:", received_message)
+        receive_can_messages(bus)
 
     except can.CanError as e:
         print("CAN error:", str(e))
 
     finally:
-        # Close the CAN bus interface
+        # Properly shut down the CAN bus interface
         bus.shutdown()
 
 if __name__ == '__main__':
     main()
-OSError: [WinError 10047] An address incompatible with the requested protocol was used
-SocketcanBus was not properly shut down
-Invoke-WebRequest -Uri "https://github.com/linklayer/cantact-app/releases/latest/download/CANtact-win64.zip" -OutFile "CANtact-win64.zip"
-
