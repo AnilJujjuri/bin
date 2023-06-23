8d0b38e0c306ba086f70725f4e1f0698925320bf
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-23 08:18:37 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,23 +1,7 @@
-import can
-import logging
-from azure.iot.device import IoTHubModuleClient
-
-logger = logging.getLogger(__name__)
-
-class CommunicationModule:
-    def __init__(self, connection_string=None, can_interface=None):
-        self.connection_string = connection_string
-        self.can_interface = can_interface
-        self.client = None
-
-    def connect(self):
-        logger.info(f"Connecting: {self.connection_string}")
-        self.client = IoTHubModuleClient.create_from_connection_string(self.connection_string)
-        self.client.connect()
-
+ def run(self):
         # Initialize CAN communication
-        bus = can.interface.Bus(channel=self.can_interface, bustype="socketcan")
-
+        #bus = can.interface.Bus(channel=self.can_interface, bustype="socketcan_native")
+        bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=500000)
         while True:
             # Receive CAN messages
             can_messages = bus.recv()
@@ -25,39 +9,48 @@ class CommunicationModule:
             # Process CAN messages
             for message in can_messages:
                 # Process the received CAN message and extract the desired data
-                data = {
-                    "can_id": message.arbitration_id,
-                    "can_data": message.data,
-                    "can_timestamp": message.timestamp,
-                }
-                print("Received CAN data:", data)
+                data = process_can_message(message)
 
-                # Send the data to device twin
-                self.send_data_to_twin(data)
+                # Update the desired properties with the extracted data
+                self.properties["desired"].update(data)
+                if self.on_property_update_callback:
+                    self.on_property_update_callback(self.properties["desired"])
 
-    def send_data_to_twin(self, data):
-        reported_properties = {"reported": data}
-        self.client.patch_twin_reported_properties(reported_properties)
+                # Update the reported properties in the device twin
+                self.update_property(data)
 
-    def disconnect(self):
-        if self.client:
-            self.client.shutdown()
+            time.sleep(1)
 
-if __name__ == "__main__":
-    # Azure IoT Hub connection string
-    connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;ModuleId=SimulatedTemperatureSensor;SharedAccessKey=v1cnONzs99unf3XKKACDfx0E69/bE5INBg/o0A7sKXw="
+def process_can_message(message):
+    # Extract the desired data from the CAN message
+    data = {
+        "can_id": message.arbitration_id,
+        "can_data": message.data,
+        "can_timestamp": message.timestamp,
+    }
+    return data
+from azure.iot.device import IoTHubModuleClient
+import logging
 
-    # CAN interface name
-    can_interface = "vcan0"
+from azurecom import CommunicationModule
+logger = logging.getLogger(__name__)
 
-    # Instantiate the CommunicationModule
-    module = CommunicationModule(connection_string=connection_string, can_interface=can_interface)
+can='vcan0'
+connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;ModuleId=SimulatedTemperatureSensor;SharedAccessKey=v1cnONzs99unf3XKKACDfx0E69/bE5INBg/o0A7sKXw="
+client = IoTHubModuleClient.create_from_connection_string(connection_string)
+logger.info(f"self.client in connect if: {client}")
+print(client)
+# properties = client.get_twin()
+# print(properties)
 
-    try:
-        # Connect to Azure IoT Hub and start listening to CAN bus
-        module.connect()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        # Disconnect from Azure IoT Hub
-        module.disconnect()
+with CommunicationModule(connection_string=connection_string) as module:
+    properties=module.get_properties()
+    print(properties)
+
+    print(module.run())
+    data_to_insert={"sensor_id":123,"temperature": 25.5,"humidity": 60.2}
+    module.update_property(data_to_insert)
+
+    properties=module.get_properties()
+    print(properties)
+
