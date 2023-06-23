f36c8934b780ddcceb19f0fd4236e03a3a5877ac
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-23 09:50:56 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,20 +1,47 @@
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
- Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
-Exception caught in background thread.  Unable to handle.
-['azure.iot.device.common.transport_exceptions.ConnectionDroppedError: Unexpected disconnection\n']
+from azure.iot.device import IoTHubDeviceClient
+import can
+
+def send_can_message(bus, can_id, data):
+    message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
+    bus.send(message)
+
+def convert_telemetry_to_candump(telemetry):
+    # Convert telemetry data to candump format
+    # Adjust the conversion logic based on your specific telemetry data structure
+    sensor_id = telemetry["sensor_id"]
+    temperature = telemetry["temperature"]
+    humidity = telemetry["humidity"]
+
+    candump = f"{sensor_id} #{temperature:X}{humidity:X}"
+
+    return candump
+
+def main():
+    # Create a CAN bus instance for the 'vcan0' interface
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+    # Create an instance of the IoTHubDeviceClient
+    device_connection_string = "<your-device-connection-string>"
+    client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
+
+    # Connect to IoT Hub
+    client.connect()
+
+    # Start receiving and processing device twin updates
+    while True:
+        twin = client.get_twin()
+        desired_properties = twin["desired"]
+        
+        # Check if there are desired properties related to telemetry
+        if "telemetry" in desired_properties:
+            telemetry = desired_properties["telemetry"]
+            candump = convert_telemetry_to_candump(telemetry)
+
+            # Print the converted telemetry data in candump format
+            print(candump)
+
+        # Wait for some time before checking for device twin updates again
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
