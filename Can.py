666e7231274ea5e82fb213ef7859c7c5255444f1
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 07:51:38 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,26 +1,54 @@
 from azure.iot.device import IoTHubDeviceClient
+import can
+import time
+def send_can_message(bus, can_id, data):
+    message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
+    bus.send(message)
 
-def send_telemetry(device_connection_string, telemetry):
+def convert_telemetry_to_candump(telemetry):
+    # Convert telemetry data to candump format
+    # Adjust the conversion logic based on your specific telemetry data structure
+    sensor_id = telemetry["sensor_id"]
+    temperature = telemetry["temperature"]
+    humidity = telemetry["humidity"]
+
+    candump = f"{sensor_id} #{temperature:X}{humidity:X}"
+
+    return candump
+
+def main(debug=False):
+    # Create a CAN bus instance for the 'vcan0' interface
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
     # Create an instance of the IoTHubDeviceClient
+    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;ModuleId=SimulatedTemperatureSensor;SharedAccessKey=v1cnONzs99unf3XKKACDfx0E69/bE5INBg/o0A7sKXw="
     client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 
     # Connect to IoT Hub
     client.connect()
+    print(client)
+    # Start receiving and processing device twin updates
+    while True:
+        twin = client.get_twin()
+        desired_properties = twin["desired"]
+        print(desired_properties)
+        # Check if there are desired properties related to telemetry
+        if "telemetry" in desired_properties:
+            telemetry = desired_properties["telemetry"]
+            candump = convert_telemetry_to_candump(telemetry)
 
-    # Send the telemetry data to the device twin
-    client.patch_twin_reported_properties(telemetry)
+            # Print the converted telemetry data in candump format
+            print(candump)
 
-    # Disconnect from IoT Hub
-    client.disconnect()
-
+        # Wait for some time before checking for device twin updates again
+        time.sleep(1)
+    print(desired_properties)
 if __name__ == '__main__':
-    # Define the device connection string and telemetry data
-    connection_string = "<your-device-connection-string>"
-    telemetry_data = {
-        "sensor_id": "001",
-        "temperature": 25.4,
-        "humidity": 63.2
-    }
+    main(debug = True)
 
-    # Send telemetry data to device twin
-    send_telemetry(connection_string, telemetry_data)
+
+<azure.iot.device.iothub.sync_clients.IoTHubDeviceClient object at 0x7f13d2547d00>
+{'SendData': True, 'SendInterval': 5, '$version': 5}
+{'SendData': True, 'SendInterval': 5, '$version': 5}
+{'SendData': True, 'SendInterval': 5, '$version': 5}
+{'SendData': True, 'SendInterval': 5, '$version': 5}
