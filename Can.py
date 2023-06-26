2e97213781749bffbb7e72e92a2fd0e2d02e62e8
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 08:15:14 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,48 +1,38 @@
-import can
-from azure.iot.device import IoTHubDeviceClient
-
-def send_can_message(bus, can_id, data):
+anilvm@Machine1:/home/azure$ python test3.py
+/usr/lib/python3/dist-packages/requests/__init__.py:89: RequestsDependencyWarning: urllib3 (2.0.3) or chardet (5.1.0) doesn't match a supported version!
+  warnings.warn("urllib3 ({}) or chardet ({}) doesn't match a supported "
+<azure.iot.device.iothub.sync_clients.IoTHubDeviceClient object at 0x7fd0dc50dd00>
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+Traceback (most recent call last):
+  File "test3.py", line 50, in <module>
+    main(debug = True)
+  File "test3.py", line 43, in main
+    send_can_message(bus, can_id, can_data)            # Print the converted telemetry data in candump format
+  File "test3.py", line 5, in send_can_message
     message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
-    bus.send(message)
+TypeError: __init__() got an unexpected keyword argument 'extended_id'
+SocketcanBus was not properly shut down
+anilvm@Machine1:/home/azure$ sudo vi test3.py
+anilvm@Machine1:/home/azure$ python test3.py
+/usr/lib/python3/dist-packages/requests/__init__.py:89: RequestsDependencyWarning: urllib3 (2.0.3) or chardet (5.1.0) doesn't match a supported version!
+  warnings.warn("urllib3 ({}) or chardet ({}) doesn't match a supported "
+<azure.iot.device.iothub.sync_clients.IoTHubDeviceClient object at 0x7f0ec99b6d00>
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.8/dist-packages/can/message.py", line 97, in __init__
+    self.data = bytearray(data)
+TypeError: 'float' object cannot be interpreted as an integer
 
-def convert_telemetry_to_candata(telemetry):
-    # Convert telemetry data to CAN data format
-    # Adjust the conversion logic based on your specific telemetry data structure
-    temperature = telemetry.get("temperature", 0)
-    humidity = telemetry.get("humidity", 0)
-    return [temperature, humidity]
+The above exception was the direct cause of the following exception:
 
-def main():
-    # Create a CAN bus instance for the 'vcan0' interface
-    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
-
-    # Create an instance of the IoTHubDeviceClient
-    device_connection_string = "<your device connection string>"
-    client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
-
-    # Connect to IoT Hub
-    client.connect()
-
-    while True:
-        # Get the device twin
-        twin = client.get_twin()
-        reported_properties = twin["reported"]
-
-        # Check if there are reported properties related to telemetry
-        if "temperature" in reported_properties and "humidity" in reported_properties:
-            temperature = reported_properties["temperature"]
-            humidity = reported_properties["humidity"]
-            can_data = [temperature, humidity]
-
-            # Send CAN message
-            can_id = 0x123  # Example CAN ID
-            send_can_message(bus, can_id, can_data)
-
-        # Wait for some time before checking for device twin updates again
-        time.sleep(1)
-
-    # Disconnect from IoT Hub
-    client.disconnect()
-
-if __name__ == '__main__':
-    main()
+Traceback (most recent call last):
+  File "test3.py", line 50, in <module>
+    main(debug = True)
+  File "test3.py", line 43, in main
+    send_can_message(bus, can_id, can_data)            # Print the converted telemetry data in candump format
+  File "test3.py", line 5, in send_can_message
+    message = can.Message(arbitration_id=can_id, data=data)
+  File "/usr/local/lib/python3.8/dist-packages/can/message.py", line 100, in __init__
+    raise TypeError(err) from error
+TypeError: Couldn't create message from [25.4, 63.2] (<class 'list'>)
+SocketcanBus was not properly shut down
