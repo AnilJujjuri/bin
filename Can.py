9d708fd492d43e4b1e5baaa9c0190f366554dcf0
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 08:08:05 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,23 +1,13 @@
-import can
-
-def send_can_message(bus, can_id, data):
-    message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
-    bus.send(message)
-
-def main():
-    # Create a CAN bus instance for the 'vcan0' interface
-    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
-
-    # Telemetry data
-    sensor_id = 0x123
-    temperature = 25
-    humidity = 60
-
-    # Construct CAN message data
-    can_data = [temperature, humidity]
-
-    # Send CAN message
-    send_can_message(bus, sensor_id, can_data)
-
-if __name__ == '__main__':
-    main()
+<azure.iot.device.iothub.sync_clients.IoTHubDeviceClient object at 0x7fe23e29ed00>
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
