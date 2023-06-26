fb5c9e0227346ea831d3808e45b91fed61fc8099
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 07:35:52 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,47 +1,26 @@
 from azure.iot.device import IoTHubDeviceClient
-import can
 
-def send_can_message(bus, can_id, data):
-    message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
-    bus.send(message)
-
-def convert_telemetry_to_candump(telemetry):
-    # Convert telemetry data to candump format
-    # Adjust the conversion logic based on your specific telemetry data structure
-    sensor_id = telemetry["sensor_id"]
-    temperature = telemetry["temperature"]
-    humidity = telemetry["humidity"]
-
-    candump = f"{sensor_id} #{temperature:X}{humidity:X}"
-
-    return candump
-
-def main():
-    # Create a CAN bus instance for the 'vcan0' interface
-    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
-
+def send_telemetry(device_connection_string, telemetry):
     # Create an instance of the IoTHubDeviceClient
-    device_connection_string = "<your-device-connection-string>"
     client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 
     # Connect to IoT Hub
     client.connect()
 
-    # Start receiving and processing device twin updates
-    while True:
-        twin = client.get_twin()
-        desired_properties = twin["desired"]
-        
-        # Check if there are desired properties related to telemetry
-        if "telemetry" in desired_properties:
-            telemetry = desired_properties["telemetry"]
-            candump = convert_telemetry_to_candump(telemetry)
+    # Send the telemetry data to the device twin
+    client.patch_twin_reported_properties(telemetry)
 
-            # Print the converted telemetry data in candump format
-            print(candump)
-
-        # Wait for some time before checking for device twin updates again
-        time.sleep(1)
+    # Disconnect from IoT Hub
+    client.disconnect()
 
 if __name__ == '__main__':
-    main()
+    # Define the device connection string and telemetry data
+    connection_string = "<your-device-connection-string>"
+    telemetry_data = {
+        "sensor_id": "001",
+        "temperature": 25.4,
+        "humidity": 63.2
+    }
+
+    # Send telemetry data to device twin
+    send_telemetry(connection_string, telemetry_data)
