bedbefe5becf95eb4b66eead8591c08d76bde823
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 08:09:28 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,13 +1,48 @@
-<azure.iot.device.iothub.sync_clients.IoTHubDeviceClient object at 0x7fe23e29ed00>
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
-{'SendData': True, 'SendInterval': 5, 'CustomProperty': 2000, 'reported': {'CustomProperty': 2000, 'sensor_id': 123, 'temperature': 25.5, 'humidity': 60.2}, 'sensor_id': '001', 'temperature': 25.4, 'humidity': 63.2, '$version': 78}
+import can
+from azure.iot.device import IoTHubDeviceClient
+
+def send_can_message(bus, can_id, data):
+    message = can.Message(arbitration_id=can_id, data=data, extended_id=False)
+    bus.send(message)
+
+def convert_telemetry_to_candata(telemetry):
+    # Convert telemetry data to CAN data format
+    # Adjust the conversion logic based on your specific telemetry data structure
+    temperature = telemetry.get("temperature", 0)
+    humidity = telemetry.get("humidity", 0)
+    return [temperature, humidity]
+
+def main():
+    # Create a CAN bus instance for the 'vcan0' interface
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+    # Create an instance of the IoTHubDeviceClient
+    device_connection_string = "<your device connection string>"
+    client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
+
+    # Connect to IoT Hub
+    client.connect()
+
+    while True:
+        # Get the device twin
+        twin = client.get_twin()
+        reported_properties = twin["reported"]
+
+        # Check if there are reported properties related to telemetry
+        if "temperature" in reported_properties and "humidity" in reported_properties:
+            temperature = reported_properties["temperature"]
+            humidity = reported_properties["humidity"]
+            can_data = [temperature, humidity]
+
+            # Send CAN message
+            can_id = 0x123  # Example CAN ID
+            send_can_message(bus, can_id, can_data)
+
+        # Wait for some time before checking for device twin updates again
+        time.sleep(1)
+
+    # Disconnect from IoT Hub
+    client.disconnect()
+
+if __name__ == '__main__':
+    main()
