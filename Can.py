3037e2c9bcf76e0904cccaf195aea4e986e4f681
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-26 09:07:54 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,12 +1,12 @@
 /usr/lib/python3/dist-packages/requests/__init__.py:89: RequestsDependencyWarning: urllib3 (2.0.3) or chardet (5.1.0) doesn't match a supported version!
   warnings.warn("urllib3 ({}) or chardet ({}) doesn't match a supported "
 Traceback (most recent call last):
-  File "test5.py", line 62, in <module>
+  File "test5.py", line 64, in <module>
     main(debug=True)
-  File "test5.py", line 53, in main
+  File "test5.py", line 55, in main
     send_can_message(bus, telemetry)
   File "test5.py", line 6, in send_can_message
     candump=convert_telemetry_to_candump(telemetry)
-  File "test5.py", line 13, in convert_telemetry_to_candump
-    temperature = telemetry["reported"]["temperature"]   # Scale and convert to integer
-KeyError: 'reported'
+  File "test5.py", line 16, in convert_telemetry_to_candump
+    candump = f"001 #{temperature:02X}{humidity:02X}"  # Assuming sensor_id is constant as '001'
+ValueError: Unknown format code 'X' for object of type 'float'
