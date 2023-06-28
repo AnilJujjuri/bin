2792ae96dc1cbb4954cb7aa57a21445c964a077a
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-28 14:44:56 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,69 +1,49 @@
 from azure.iot.device import IoTHubDeviceClient
 import can
-import time
 
 def send_can_message(bus, can_id, data):
     message = can.Message(arbitration_id=can_id, data=data)
     bus.send(message)
 
-def convert_telemetry_to_candump(sensor_id, temperature, humidity):
+def convert_telemetry_to_candump(sensor_id, telemetry_data):
     # Convert telemetry data to candump format
     # Adjust the conversion logic based on your specific telemetry data structure
-    temperature = int(temperature * 10)  # Scale and convert to integer
-    humidity = int(humidity * 10)  # Scale and convert to integer
-    sensor_id = int(sensor_id * 10)
-    candump = f"{sensor_id:02X} #{temperature:02X}{humidity:02X}"  # Assuming sensor_id is constant as '001'
 
-    return candump
+    candump = f"{sensor_id}_"
 
-def main(debug=False):
-    # Create a CAN bus instance for the 'vcan0' interface
+    for key, value in telemetry_data.items():
+        if isinstance(value, int):
+            value = int(value)  # Convert to integer
+
+        candump += f"{key}_{value}_"  # Append key-value pair to the candump string
+
+    return candump.rstrip("_")  # Remove the trailing underscore
+
+def handle_device_twin_update(twin, bus):
+    reported_properties = twin["reported"]
+
+    for sensor_id, telemetry_data in reported_properties.items():
+        if isinstance(telemetry_data, dict):
+            candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
+            can_id, can_data = candump.split("_")
+            can_id = can_id.strip()
+            can_data = [int(byte) for byte in can_data.split("_")]
+
+            send_can_message(bus, int(can_id), can_data)
+
+def main():
     bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
 
-    # Create an instance of the IoTHubDeviceClient
-    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;ModuleId=SimulatedTemperatureSensor;SharedAccessKey=v1cnONzs99unf3XKKACDfx0E69/bE5INBg/o0A7sKXw="
+    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;SharedAccessKey=mOeGufRBpvjmFut51ghJ0gjmWZDR8BHN1WWJtdsrBY4="
     client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 
-    # Connect to IoT Hub
     client.connect()
 
-    # Initialize the flag to track if the message has been sent
-    message_sent = False
-
-    # Start receiving and processing device twin updates
     while True:
         twin = client.get_twin()
-        reported_properties = twin["reported"]
+        handle_device_twin_update(twin, bus)
 
-        # Check if there are reported properties related to temperature, humidity, and sensor_id
-        if "temperature" in reported_properties and "humidity" in reported_properties and "sensor_id" in reported_properties:
-            temperature = reported_properties["temperature"]
-            humidity = reported_properties["humidity"]
-            sensor_id = reported_properties["sensor_id"]
-
-            # If the message has not been sent, send it
-            if not message_sent:
-                candump = convert_telemetry_to_candump(sensor_id, temperature, humidity)
-                can_id, can_data = candump.split("#")
-                can_id = can_id.strip()  # Remove extra whitespace
-                can_data = [int(can_data[i:i+2], 16) for i in range(0, len(can_data), 2)]
-                send_can_message(bus, int(sensor_id), can_data)
-                message_sent = True
-
-        # If the message has been sent, update the data
-        if message_sent:
-            # Update the telemetry data as needed
-            temperature += 1.0
-            humidity += 1.0
-
-        # Reset the flag to allow sending a new message in the next iteration
-        message_sent = False
-
-        # Wait for some time before checking for device twin updates again
-        time.sleep(1)
-
-    # Disconnect from IoT Hub
     client.disconnect()
 
 if __name__ == '__main__':
-    main(debug=True)
+    main()
