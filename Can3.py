bf3f51ac0f66e7ebbaea03bf6e070e29fee1c7a6
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-28 15:37:31 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -29,7 +29,7 @@ def handle_device_twin_update(twin, bus):
                 can_id = can_id_parts[1]  # The numeric part is the can_id
 
                 candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
-                can_data = [int(byte) for byte in candump.split("_")[1:] if byte.isnumeric()]  # Skip the first part if numeric
+                can_data = [int(byte) % 256 for byte in candump.split("_")[1:] if byte.isnumeric()]  # Convert and limit values to valid range
 
                 send_can_message(bus, int(can_id), can_data)
 
@@ -49,17 +49,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-Traceback (most recent call last):
-  File "/home/azure/generic.py", line 51, in <module>
-    main()
-  File "/home/azure/generic.py", line 46, in main
-    handle_device_twin_update(twin, bus)
-  File "/home/azure/generic.py", line 34, in handle_device_twin_update
-    send_can_message(bus, int(can_id), can_data)
-  File "/home/azure/generic.py", line 5, in send_can_message
-    message = can.Message(arbitration_id=can_id, data=data)
-  File "/home/Machine1/.local/lib/python3.8/site-packages/can/message.py", line 97, in __init__
-    self.data = bytearray(data)
-ValueError: byte must be in range(0, 256)
-SocketcanBus was not properly shut down
