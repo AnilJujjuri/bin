61087a61d9c1af119907a1b5d4300e907cca8dfc
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 15:17:10 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,21 +1,11 @@
-def convert_telemetry_to_candump(sensor_id, telemetry_data):
-    candump = f"{sensor_id}_"
-
-    for key, value in telemetry_data.items():
-        if isinstance(value, int):
-            value = int(value)
-        elif isinstance(value, float):
-            value = int(value * 100)
-        elif isinstance(value, str):
-            try:
-                value = int(value)
-            except ValueError:
-                try:
-                    value = float(value)
-                except ValueError:
-                    continue  # Skip this key-value pair if conversion is not possible
-
-        value = max(min(value, 255), 0)
-        candump += f"{key}_{value}_"
-
-    return candump.rstrip("_")
+Traceback (most recent call last):
+  File "/home/azure/test11.py", line 69, in <module>
+    main(debug=True)
+  File "/home/azure/test11.py", line 59, in main
+    handle_device_twin_update(twin, bus)
+  File "/home/azure/test11.py", line 47, in handle_device_twin_update
+    send_can_message(bus, int(can_id), can_data)
+  File "/home/azure/test11.py", line 7, in send_can_message
+    message_data=bytes(data)
+ValueError: bytes must be in range(0, 256)
+SocketcanBus was not properly shut down
