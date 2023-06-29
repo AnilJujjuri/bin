517f194166298282c43a6abce65e362b18d0218e
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 14:56:33 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,57 +1,25 @@
-from azure.iot.device import IoTHubDeviceClient
-import can
-import time
-def send_can_message(bus, can_id, data):
-    message = can.Message(arbitration_id=can_id, data=data)
-    bus.send(message)
-
-def convert_telemetry_to_candump(sensor_id, telemetry_data):
-    # Convert telemetry data to candump format
-    # Adjust the conversion logic based on your specific telemetry data structure
-
-    candump = f"{sensor_id}_"
-
-    for key, value in telemetry_data.items():
-        if isinstance(value, int):
-            value = int(value)  # Convert to integer
-
-        candump += f"{key}_{value}_"  # Append key-value pair to the candump string
-
-    return candump.rstrip("_")  # Remove the trailing underscore
-
 def handle_device_twin_update(twin, bus):
     reported_properties = twin["reported"]
 
     for sensor_id, telemetry_data in reported_properties.items():
         if isinstance(telemetry_data, dict):
-            can_id_parts = sensor_id.split("_")  # Split the sensor_id to get the parts
+            can_id_parts = sensor_id.split("_")
             if len(can_id_parts) >= 2 and can_id_parts[1].isnumeric():
-                can_id = can_id_parts[1]  # The numeric part is the can_id
+                can_id = can_id_parts[1]
 
                 candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
-                can_data = [int(byte) % 256 for byte in candump.split("_")[1:] if byte.isnumeric()]  # Convert and limit values to valid range
+
+                can_data = []
+                for byte in candump.split("_")[1:]:
+                    if byte.isnumeric():
+                        can_data.append(int(byte) % 256)
+                    elif "." in byte:
+                        float_value = float(byte)
+                        float_bytes = struct.pack('!f', float_value)
+                        float_int = struct.unpack('!I', float_bytes)[0]
+                        can_data.append(float_int % 256)
+                        can_data.append((float_int >> 8) % 256)
+                        can_data.append((float_int >> 16) % 256)
+                        can_data.append((float_int >> 24) % 256)
 
                 send_can_message(bus, int(can_id), can_data)
-                
-
-def main(debug=False):
-    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
-
-    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;SharedAccessKey=mOeGufRBpvjmFut51ghJ0gjmWZDR8BHN1WWJtdsrBY4="
-    client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
-
-    client.connect()
-    retry_counter = 0 
-    while True:
-        twin = client.get_twin()
-        handle_device_twin_update(twin, bus)
-        # Retry mechanism
-        if retry_counter < 3:
-            time.sleep(100)  # Wait for 10 seconds between retries
-            retry_counter += 1
-        else:
-            break  # Disconnect after the maximum number of retries
-        client.disconnect()
-        break
-if __name__ == '__main__':
-    main(debug=True)
