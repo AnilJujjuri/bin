af41952022d84e1de7b50f4f6f3199566881158f
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 15:08:25 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,12 +1,10 @@
 Traceback (most recent call last):
-  File "/home/azure/test11.py", line 60, in <module>
+  File "/home/azure/test11.py", line 61, in <module>
     main(debug=True)
-  File "/home/azure/test11.py", line 50, in main
+  File "/home/azure/test11.py", line 51, in main
     handle_device_twin_update(twin, bus)
-  File "/home/azure/test11.py", line 38, in handle_device_twin_update
+  File "/home/azure/test11.py", line 39, in handle_device_twin_update
     send_can_message(bus, int(can_id), can_data)
   File "/home/azure/test11.py", line 7, in send_can_message
-    message = can.Message(arbitration_id=can_id, data=data)
-  File "/home/Machine1/.local/lib/python3.8/site-packages/can/message.py", line 97, in __init__
-    self.data = bytearray(data)
-ValueError: byte must be in range(0, 256)
+    message_data=bytes(data)
+ValueError: bytes must be in range(0, 256)
