a0c9470e2c5f6ba76adfaa4fde1d411510c7e350
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 12:06:09 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,35 +1,57 @@
 from azure.iot.device import IoTHubDeviceClient
+import can
+import time
+def send_can_message(bus, can_id, data):
+    message = can.Message(arbitration_id=can_id, data=data)
+    bus.send(message)
 
-def send_telemetry(device_connection_string, telemetry_data):
-    # Create an instance of the IoTHubDeviceClient
+def convert_telemetry_to_candump(sensor_id, telemetry_data):
+    # Convert telemetry data to candump format
+    # Adjust the conversion logic based on your specific telemetry data structure
+
+    candump = f"{sensor_id}_"
+
+    for key, value in telemetry_data.items():
+        if isinstance(value, int):
+            value = int(value)  # Convert to integer
+
+        candump += f"{key}_{value}_"  # Append key-value pair to the candump string
+
+    return candump.rstrip("_")  # Remove the trailing underscore
+
+def handle_device_twin_update(twin, bus):
+    reported_properties = twin["reported"]
+
+    for sensor_id, telemetry_data in reported_properties.items():
+        if isinstance(telemetry_data, dict):
+            can_id_parts = sensor_id.split("_")  # Split the sensor_id to get the parts
+            if len(can_id_parts) >= 2 and can_id_parts[1].isnumeric():
+                can_id = can_id_parts[1]  # The numeric part is the can_id
+
+                candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
+                can_data = [int(byte) % 256 for byte in candump.split("_")[1:] if byte.isnumeric()]  # Convert and limit values to valid range
+
+                send_can_message(bus, int(can_id), can_data)
+                
+
+def main(debug=False):
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;SharedAccessKey=mOeGufRBpvjmFut51ghJ0gjmWZDR8BHN1WWJtdsrBY4="
     client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 
-    # Connect to IoT Hub
     client.connect()
-
-    # Send the telemetry data to the device twin
-    client.patch_twin_reported_properties(telemetry_data)
-
-    # Disconnect from IoT Hub
-    client.disconnect()
-
+    retry_counter = 0 
+    while True:
+        twin = client.get_twin()
+        handle_device_twin_update(twin, bus)
+        # Retry mechanism
+        if retry_counter < 3:
+            time.sleep(100)  # Wait for 10 seconds between retries
+            retry_counter += 1
+        else:
+            break  # Disconnect after the maximum number of retries
+        client.disconnect()
+        break
 if __name__ == '__main__':
-    # Define the device connection string and telemetry data
-    connection_string = "HostName=your-iot-hub.azure-devices.net;DeviceId=your-device-id;SharedAccessKey=your-shared-access-key"
-    telemetry_data = {
-        "sensor": "unique_key",
-        "temperature": 25.5,
-        "humidity": 60.2
-    }
-
-    # Send telemetry data to device twin
-    send_telemetry(connection_string, telemetry_data)
-
-
-ERROR: for integration340_mender-api-gateway_1  Cannot start service mender-api-gateway: driver failed programming external connectivity on endpoint integration340_mender-api-gateway_1 (3184c328ae4635af67630a0030642a59ab5700e15e6e9fa7806467f3d267d9d3): Bind for 0.0.0.0:443 failed: port is already allocated
-
- 
-
-ERROR: for mender-api-gateway  Cannot start service mender-api-gateway: driver failed programming external connectivity on endpoint integration340_mender-api-gateway_1 (3184c328ae4635af67630a0030642a59ab5700e15e6e9fa7806467f3d267d9d3): Bind for 0.0.0.0:443 failed: port is already allocated
-ERROR: Encountered errors while bringing up the project.
-Failed to start docker compose
+    main(debug=True)
