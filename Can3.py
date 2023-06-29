5f3d61bb3f86ab0f7efd863c8ebcac5f12abac10
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 15:27:21 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,11 +1,56 @@
-Traceback (most recent call last):
-  File "/home/azure/test11.py", line 69, in <module>
+from azure.iot.device import IoTHubDeviceClient
+import can
+import time
+
+def send_can_message(bus, can_id, data):
+    can_data = [byte % 256 for byte in data]
+    message = can.Message(arbitration_id=can_id, data=can_data)
+    bus.send(message)
+
+def convert_telemetry_to_candump(sensor_id, telemetry_data):
+    # Convert telemetry data to candump format
+    # Adjust the conversion logic based on your specific telemetry data structure
+    candump = f"{sensor_id}_"
+    for key, value in telemetry_data.items():
+        if isinstance(value, int):
+            value = int(value)  # Convert to integer
+        elif isinstance(value, float):
+            value = round(value, 2)  # Round float value to 2 decimal places
+        else:
+            continue  # Skip unsupported data type
+        candump += f"{key}_{value}_"  # Append key-value pair to the candump string
+    return candump.rstrip("_")  # Remove the trailing underscore
+
+def handle_device_twin_update(twin, bus):
+    reported_properties = twin["reported"]
+
+    for sensor_id, telemetry_data in reported_properties.items():
+        if isinstance(telemetry_data, dict):
+            can_id_parts = sensor_id.split("_")  # Split the sensor_id to get the parts
+            if len(can_id_parts) >= 2 and can_id_parts[1].isnumeric():
+                can_id = int(can_id_parts[1])  # The numeric part is the can_id
+                candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
+                can_data = [byte for byte in candump.encode()]
+                send_can_message(bus, can_id, can_data)
+
+def main(debug=False):
+    bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
+
+    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;SharedAccessKey=mOeGufRBpvjmFut51ghJ0gjmWZDR8BHN1WWJtdsrBY4="
+    client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
+
+    client.connect()
+    retry_counter = 0 
+    while True:
+        twin = client.get_twin()
+        handle_device_twin_update(twin, bus)
+        # Retry mechanism
+        if retry_counter < 3:
+            time.sleep(10)  # Wait for 10 seconds between retries
+            retry_counter += 1
+        else:
+            break  # Disconnect after the maximum number of retries
+    client.disconnect()
+
+if __name__ == '__main__':
     main(debug=True)
-  File "/home/azure/test11.py", line 59, in main
-    handle_device_twin_update(twin, bus)
-  File "/home/azure/test11.py", line 47, in handle_device_twin_update
-    send_can_message(bus, int(can_id), can_data)
-  File "/home/azure/test11.py", line 7, in send_can_message
-    message_data=bytes(data)
-ValueError: bytes must be in range(0, 256)
-SocketcanBus was not properly shut down
