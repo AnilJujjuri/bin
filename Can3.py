4a131e07f500afb27eed2c437668a36614fcb6fe
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-06-29 07:15:07 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,4 +1,3 @@
-import time
 from azure.iot.device import IoTHubDeviceClient
 import can
 
@@ -21,36 +20,33 @@ def convert_telemetry_to_candump(sensor_id, telemetry_data):
     return candump.rstrip("_")  # Remove the trailing underscore
 
 def handle_device_twin_update(twin, bus):
-    reported_properties = twin.get("reported")
+    reported_properties = twin["reported"]
 
-    if reported_properties is not None:
-        for sensor_id, telemetry_data in reported_properties.items():
-            if isinstance(telemetry_data, dict):
-                can_id_parts = sensor_id.split("_")  # Split the sensor_id to get the parts
-                if len(can_id_parts) >= 2 and can_id_parts[1].isnumeric():
-                    can_id = can_id_parts[1]  # The numeric part is the can_id
+    for sensor_id, telemetry_data in reported_properties.items():
+        if isinstance(telemetry_data, dict):
+            can_id_parts = sensor_id.split("_")  # Split the sensor_id to get the parts
+            if len(can_id_parts) >= 2 and can_id_parts[1].isnumeric():
+                can_id = can_id_parts[1]  # The numeric part is the can_id
 
-                    candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
-                    can_data = [int(byte) % 256 for byte in candump.split("_")[1:] if byte.isnumeric()]  # Convert and limit values to valid range
+                candump = convert_telemetry_to_candump(sensor_id, telemetry_data)
+                can_data = [int(byte) % 256 for byte in candump.split("_")[1:] if byte.isnumeric()]  # Convert and limit values to valid range
 
-                    send_can_message(bus, int(can_id), can_data)
+                send_can_message(bus, int(can_id), can_data)
+                
 
 def main():
     bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
 
-    device_connection_string = "HostName=your-iot-hub.azure-devices.net;DeviceId=your-device-id;SharedAccessKey=your-shared-access-key"
+    device_connection_string = "HostName=EDGTneerTrainingPractice.azure-devices.net;DeviceId=nodered;SharedAccessKey=mOeGufRBpvjmFut51ghJ0gjmWZDR8BHN1WWJtdsrBY4="
     client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 
     client.connect()
 
     while True:
-        twin_updates = client.receive_twin_desired_properties_patch()  # Check for new twin updates
-        if twin_updates:
-            handle_device_twin_update(twin_updates, bus)
-        
-        time.sleep(1)  # Sleep for 1 second
+        twin = client.get_twin()
+        handle_device_twin_update(twin, bus)
 
-    client.disconnect()
+        client.disconnect()
 
 if __name__ == '__main__':
     main()
