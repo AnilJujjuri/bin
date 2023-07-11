34931a1e120e21e60724db07654a60457faa2d1e
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-07-11 09:26:09 +0530

    Update Can.py

--- Can.py
+++ Can.py
@@ -1,1 +1,23 @@
-Received CAN message: Timestamp: 1689047647.213762    ID: 0000027b    X Tx                DL:  3    1f 81 7a                    Channel: vcan0
+import csv
+
+def receive_can_messages(bus):
+    csv_file = 'received_can_messages.csv'
+    file_exists = os.path.isfile(csv_file)
+    with open(csv_file, 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        if not file_exists:  # If the file doesn't exist, write the header
+            writer.writerow(['Timestamp', 'ID', 'Direction', 'DLC', 'Data', 'Channel'])  # Write the header row
+
+        while True:
+            message = bus.recv()
+            timestamp = message.timestamp
+            can_id = message.arbitration_id
+            direction = 'Rx' if message.is_received else 'Tx'
+            dlc = message.dlc
+            data = ' '.join([f'{byte:02X}' for byte in message.data])
+            channel = bus.channel_info
+
+            writer.writerow([timestamp, can_id, direction, dlc, data, channel])  # Write the data row
+
+            print(f"Received CAN message: Timestamp: {timestamp}    ID: {can_id:08X}    {direction:<4}                DL: {dlc:<2}    {data:26}    Channel: {channel}")
