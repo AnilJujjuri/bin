46679d96b6c78d8df4f705357ad1d8233132ac59
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-08-31 23:21:27 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -3,7 +3,5 @@ Traceback (most recent call last):
   File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 17, in <module>
     run_modbus_server()
   File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 14, in run_modbus_server
-    StartTcpServer(context, address=("0.0.0.0", 5020))
+    server = StartTcpServer(context, address=("0.0.0.0", 5020))
 TypeError: StartTcpServer() takes 0 positional arguments but 1 was given
-
-Process finished with exit code 1
