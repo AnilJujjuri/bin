69c0b5fdc7369aff9ce5bd98a3c745598bc390e0
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-08-31 23:40:31 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,17 +1,14 @@
-from pymodbus3.server.sync import StartTcpServer
-from pymodbus3.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus3.datastore import ModbusSequentialDataBlock
+Traceback (most recent call last):
+  File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 1, in <module>
+    from pymodbus3.server.sync import StartTcpServer
+  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\server\sync.py", line 12, in <module>
+    from pymodbus3.factory import ServerDecoder
+  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\factory.py", line 16, in <module>
+    from pymodbus3.pdu import IllegalFunctionRequest
+  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\pdu.py", line 8, in <module>
+    from pymodbus3.utilities import rtu_frame_size
+  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\utilities.py", line 10, in <module>
+    from collections import Callable
+ImportError: cannot import name 'Callable' from 'collections' (C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\lib\collections\__init__.py)
 
-def run_modbus_server():
-    store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [0] * 100),  # Discrete Inputs
-        co=ModbusSequentialDataBlock(0, [0] * 100),  # Coils
-        hr=ModbusSequentialDataBlock(0, [0] * 100),  # Holding Registers
-        ir=ModbusSequentialDataBlock(0, [0] * 100)   # Input Registers
-    )
-    context = ModbusServerContext(slaves=store, single=True)
-
-    server = StartTcpServer(context, address=("0.0.0.0", 5020))
-
-if __name__ == "__main__":
-    run_modbus_server()
+Process finished with exit code 1
