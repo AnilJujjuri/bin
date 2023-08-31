cf9c0a0993bed6dc654f0d6b3894f61636c63f9a
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-08-31 23:18:52 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,17 +1,9 @@
-from pymodbus.server.sync import StartTcpServer
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.datastore import ModbusSequentialDataBlock
-
-def run_modbus_server():
-    store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [0]*100),  # Discrete Inputs
-        co=ModbusSequentialDataBlock(0, [0]*100),  # Coils
-        hr=ModbusSequentialDataBlock(0, [0]*100),  # Holding Registers
-        ir=ModbusSequentialDataBlock(0, [0]*100)   # Input Registers
-    )
-    context = ModbusServerContext(slaves=store, single=True)
-
-    StartTcpServer(context, address=("0.0.0.0", 5020))
-
-if __name__ == "__main__":
+C:\Users\40020507\AppData\Local\Microsoft\WindowsApps\python3.10.exe C:/Users/40020507/Downloads/diagslave-3.4/win/tets1.py
+Traceback (most recent call last):
+  File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 17, in <module>
     run_modbus_server()
+  File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 14, in run_modbus_server
+    StartTcpServer(context, address=("0.0.0.0", 5020))
+TypeError: StartTcpServer() takes 0 positional arguments but 1 was given
+
+Process finished with exit code 1
