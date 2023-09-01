d8ea1fa589863489e8a261287d355a796be9d395
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-09-01 06:49:20 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,14 +1,14 @@
-Traceback (most recent call last):
-  File "C:\Users\40020507\Downloads\diagslave-3.4\win\tets1.py", line 1, in <module>
-    from pymodbus3.server.sync import StartTcpServer
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\server\sync.py", line 12, in <module>
-    from pymodbus3.factory import ServerDecoder
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\factory.py", line 16, in <module>
-    from pymodbus3.pdu import IllegalFunctionRequest
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\pdu.py", line 8, in <module>
-    from pymodbus3.utilities import rtu_frame_size
-  File "C:\Users\40020507\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\pymodbus3\utilities.py", line 10, in <module>
-    from collections import Callable
-ImportError: cannot import name 'Callable' from 'collections' (C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\lib\collections\__init__.py)
+from minimalmodbus import ModbusTcpServer
 
-Process finished with exit code 1
+def run_modbus_server():
+    server_ip = "0.0.0.0"  # Listen on all available interfaces
+    server_port = 5020
+
+    try:
+        modbus_server = ModbusTcpServer(server_ip, server_port)
+        modbus_server.serve_forever()
+    except KeyboardInterrupt:
+        modbus_server.close()
+
+if __name__ == "__main__":
+    run_modbus_server()
