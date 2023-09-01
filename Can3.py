20e20f2534bb5501860c61afd6c4d665b08c9c93
AnilJujjuri <90301526+AnilJujjuri@users.noreply.github.com>
2023-09-01 07:09:19 +0530

    Update Can3.py

--- Can3.py
+++ Can3.py
@@ -1,14 +1,21 @@
-from minimalmodbus import ModbusTcpServer
+from modbus_tk import modbus_tcp
+from modbus_tk.defines import READ_HOLDING_REGISTERS, WRITE_SINGLE_REGISTER
+import logging
 
 def run_modbus_server():
-    server_ip = "0.0.0.0"  # Listen on all available interfaces
-    server_port = 5020
+    server = modbus_tcp.TcpServer(address="0.0.0.0", port=5020)
+    logger = modbus_tk.utils.create_logger(name="console", record_format="%(message)s")
+    logger.setLevel(logging.INFO)
 
     try:
-        modbus_server = ModbusTcpServer(server_ip, server_port)
-        modbus_server.serve_forever()
+        logger.info("Modbus server is running...")
+        server.start()
+        slave = server.add_slave(1)
+        slave.add_block("holding", READ_HOLDING_REGISTERS, 0, 100)
+        slave.add_block("coil", WRITE_SINGLE_REGISTER, 0, 100)
+        server.start()
     except KeyboardInterrupt:
-        modbus_server.close()
+        server.stop()
 
 if __name__ == "__main__":
     run_modbus_server()
